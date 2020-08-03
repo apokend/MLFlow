9cd8636d08a2a6c81b8c0e5b93870c91e22ced8c
apokend <apokend@gmail.com>
2020-08-03 15:19:48 +0300

    new

--- main.py
+++ main.py
@@ -2,30 +2,47 @@ from argparse import ArgumentParser
 import mlflow
 from mlflow.utils import mlflow_tags
 from server.RemoteServer import RemoteTracking
-
+import os
 
 class FlowTraining:
     def __init__(self, tracking_uri, experiment_name):
         self.tracking_uri = tracking_uri
         self.experiment_name = experiment_name
-        #self.remote_server = RemoteTracking(tracking_uri=tracking_uri)
-        #self.local_experiment_dir = './mlruns'
-        #self.local_experiment_id = '0'
+        self.remote_server = RemoteTracking(tracking_uri=tracking_uri)
+        self.local_experiment_dir = './mlruns'
+        self.local_experiment_id = '0'
+
+    def log_tags_and_params(self, remote_run_id):
+        run_id = self.get_local_run_id()
+        mlflow.set_tracking_uri(self.local_experiment_dir)
+        run = mlflow.get_run(run_id = run_id)
+        params = run.data.params
+        tags = run.data.tags
+        self.remote_server.set_tags(remote_run_id, tags)
+        self.remote_server.log_params(remote_run_id, params)
+
+    def get_local_run(self):
+        files = os.listdir(os.path.join(self.local_experiment_dir, self.local_experiment_id))
+        for file in files:
+            if not file.endswith('.yaml'):
+                return file
 
     def run(self):
         print('Entry point: Workflow')
-        #remote_experiment_id = self.remote_server.get_experiment_id(name = self.experiment_name)
-        #remote_run_id = self.remote_server.get_run_id(remote_experiment_id)
+        remote_experiment_id = self.remote_server.get_experiment_id(name = self.experiment_name)
+        remote_run_id = self.remote_server.get_run_id(remote_experiment_id)
 
-        #mlflow.set_tracking_uri(self.tracking_uri)
-        #mlflow.set_experiment(self.experiment_name)
-
-        #print(remote_run_id)
+        mlflow.set_tracking_uri(self.tracking_uri)
+        mlflow.set_experiment(self.experiment_name)
 
         with mlflow.start_run() as active_run:
             git_commit = active_run.data.tags.get(mlflow_tags.MLFLOW_GIT_COMMIT)
             print(git_commit)
-            mlflow.log_param("param2", 'Hello World!')
+        try:
+            self.log_tags_and_params(remote_run_id)
+        except  mlflow.exceptions.RestException as e:
+            print(e)
+
 
 if __name__ == '__main__':
     print('Entry_point: main.py')
