21bbe7c247e9fdbdd6dfa3d16f035ea0c944274f
apokend <apokend@gmail.com>
2020-08-03 15:01:29 +0300

    new

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ class FlowTraining:
         self.tracking_uri = tracking_uri
         self.experiment_name = experiment_name
         self.remote_server = RemoteTracking(tracking_uri=tracking_uri)
-        self.local_experiment_dir = './mlruns'
-        self.local_experiment_id = '0'
+        #self.local_experiment_dir = './mlruns'
+        #self.local_experiment_id = '0'
 
     def run(self):
         print('Entry point: Workflow')
@@ -23,14 +23,15 @@ class FlowTraining:
         with mlflow.start_run(run_id = remote_run_id) as active_run:
             git_commit = active_run.data.tags.get(mlflow_tags.MLFLOW_GIT_COMMIT)
             print(git_commit)
-            mlflow.log_param("param1", 'Hello World!')
+            mlflow.log_param("param2", 'Hello World!')
 
 if __name__ == '__main__':
     print('Entry_point: main.py')
     parser = ArgumentParser()
-    parser.add_argument('-name', '--experiment_name', default ='Default', type = str)
-    parser.add_argument('-uri', '--tracking_uri', default = '127.0.0.1', type = str)
+    parser.add_argument('--experiment_name', default ='Default', type = str)
+    parser.add_argument('--tracking_uri', default = 'http://127.0.0.1:5000', type = str)
     args = parser.parse_args()
+    print(args)
     tracking = FlowTraining(experiment_name=args.experiment_name,
                             tracking_uri=args.tracking_uri)
     tracking.run()
