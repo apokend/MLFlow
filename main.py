26577e17caa900e34d49e7258967ea314030b311
apokend <apokend@gmail.com>
2020-08-03 13:47:14 +0300

    add tracking entrypoints

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from argparse import ArgumentParser
 import mlflow
+from mlflow.utils import mlflow_tags
 
 def workflow(args):
+    print('Entry point: Workflow')
     with mlflow.start_run() as active_run:
         git_commit = active_run.data.tags.get(mlflow_tags.MLFLOW_GIT_COMMIT)
         print(git_commit)
 
-
 if __name__ == '__main__':
+    print('Entry_point: main.py')
     parser = ArgumentParser()
     parser.add_argument('-name', '--experiment_name', default ='Default', type = str)
     args = parser.parse_args()
